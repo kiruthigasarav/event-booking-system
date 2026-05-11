db2279daf039e465e54de6280954d750affbb26d
S. Kiruthiga <25pite11@ldc.edu.in>
2026-05-11 20:08:55 +0530

    fixed mysql railway env

--- backend/db.js
+++ backend/db.js
@@ -10,9 +10,9 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed:", err);
+    console.log("Database Connection Error:", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("MySQL Connected Successfully");
   }
 });
 
